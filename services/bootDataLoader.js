16055300b01ff872b724e40b0b7101423030aa27
Josias <josiasmc@emypeople.net>
2021-03-05 12:22:39 -0600

    Cambiar plantillas de correo a español

--- services/bootDataLoader.js
+++ services/bootDataLoader.js
@@ -11,23 +11,23 @@ class BootDataLoader {
 
   create() {
     var that = this;
-      var email = process.env.ADMIN_EMAIL || "admin@tiledesk.com";
-      var password = process.env.ADMIN_PASSWORD || "adminadmin";
-      userService.signup(email, password, "Administrator", " ", true)
+    var email = process.env.ADMIN_EMAIL || "admin@tiledesk.com";
+    var password = process.env.ADMIN_PASSWORD || "adminadmin";
+    userService.signup(email, password, "Administrator", " ", true)
       .then(function (savedUser) {
-        winston.info("Created admin user with email "+ email + " and password "+ password);
+        winston.info("Created admin user with email " + email + " and password " + password);
         projectService.create("default", savedUser.id, undefined).then(function (savedProject) {
           winston.debug("Created default project");
-        }).catch(function(err) {
+        }).catch(function (err) {
           winston.error("Error creating default project ", err);
         });
-      }).catch(function(err) {
+      }).catch(function (err) {
         if (err.code == 11000) {
           winston.info("Admin user already exists");
-        }else {
+        } else {
           winston.error("Error creating initial data ", err);
         }
-    }); 
+      });
   }
 
 
